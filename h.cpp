44b698d5f766c806cf64ebc110e8857c15f74bd3
sanjay <paharisanjay10@gmail.com>
2023-08-14 08:44:05 +0545

    init

---
+++ h.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main()
+{
+
+    std::cout << "hello";
+    std::cout << "World";
+    std::cin.get();
+
+    return 0;
+
+}
---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    int a, b;  // Declare variables a and b to store the input integers
+
+    std::cin >> a >> b;  // Read two integers from the standard input
+    std::cout << a + b;  // Output the sum of the two integers
+    
+    return 0;  // Indicate successful completion of the program
+}
